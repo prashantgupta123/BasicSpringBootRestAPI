42995e25694e00550e5e28fc4efba59841aa17f7
prashantgupta123 <prashantgupta036@gmail.com>
2017-12-19 10:58:55 +0530

    Refactor code

--- src/main/java/com/basicSpringBootRestAPI/config/CustomBasicAuthenticationFilter.java
+++ src/main/java/com/basicSpringBootRestAPI/config/CustomBasicAuthenticationFilter.java
@@ -1,25 +1,21 @@
 package com.basicSpringBootRestAPI.config;
 
 import com.basicSpringBootRestAPI.entity.User;
-import com.basicSpringBootRestAPI.entity.UserAuthenticationToken;
-import com.basicSpringBootRestAPI.repository.UserAuthenticationTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.UUID;
 
 @Component
 public class CustomBasicAuthenticationFilter extends BasicAuthenticationFilter {
 
     @Autowired
-    private UserAuthenticationTokenRepository userAuthenticationTokenRepository;
+    private SpringSecurityService springSecurityService;
 
     public CustomBasicAuthenticationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
@@ -27,15 +23,9 @@ public class CustomBasicAuthenticationFilter extends BasicAuthenticationFilter {
 
     @Override
     protected void onSuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, Authentication authResult) throws IOException {
-        User user = (User) authResult.getPrincipal();
-        //Generate Token
-        String token = UUID.randomUUID().toString().toUpperCase() + "|" + user.getId();
-        //Save the token for the logged in user
-        UserAuthenticationToken verificationToken = new UserAuthenticationToken();
-        verificationToken.setToken(token);
-        verificationToken.setUser(user);
-        userAuthenticationTokenRepository.save(verificationToken);
-        //send token in the response
-        response.setHeader("oauth-token", "token");
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult.getPrincipal();
+        User user1 = springSecurityService.findByUsername(user.getUsername());
+        String token = springSecurityService.saveUserAuthenticationToken(user1);
+        response.setHeader("oauth-token", token);
     }
 }
--- src/main/java/com/basicSpringBootRestAPI/config/SpringSecurityService.java
+++ src/main/java/com/basicSpringBootRestAPI/config/SpringSecurityService.java
@@ -1,19 +1,38 @@
 package com.basicSpringBootRestAPI.config;
 
 import com.basicSpringBootRestAPI.entity.User;
+import com.basicSpringBootRestAPI.entity.UserAuthenticationToken;
+import com.basicSpringBootRestAPI.repository.UserAuthenticationTokenRepository;
 import com.basicSpringBootRestAPI.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class SpringSecurityService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private UserAuthenticationTokenRepository userAuthenticationTokenRepository;
 
     public User getCurrentUser() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userRepository.findByUsername(user.getUsername());
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public String saveUserAuthenticationToken(User user) {
+        String token = UUID.randomUUID().toString().toUpperCase() + "|" + user.getId();
+        UserAuthenticationToken verificationToken = new UserAuthenticationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        userAuthenticationTokenRepository.save(verificationToken);
+        return token;
     }
 }
