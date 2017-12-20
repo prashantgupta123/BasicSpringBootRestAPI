3870862429430bc49f199694da2f98b4543b22a1
prashantgupta123 <prashantgupta036@gmail.com>
2017-12-20 15:17:18 +0530

    token authentication processed

--- src/main/java/com/basicSpringBootRestAPI/config/SpringSecurityConfig.java
+++ src/main/java/com/basicSpringBootRestAPI/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.basicSpringBootRestAPI.config;
 
+import com.basicSpringBootRestAPI.enums.UserRoleEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,7 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,30 +18,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
+    @Autowired
+    private TokenAuthenticationService tokenAuthenticationService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    /*@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder managerBuilder) throws Exception {
-        managerBuilder
-                .inMemoryAuthentication()
-                .withUser("user").password("pass").roles("USER").and()
-                .withUser("admin").password("pass").roles("ADMIN");
-    }*/
-
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-
-        //Implementing Token based authentication in this filter
-        final TokenAuthenticationFilter tokenFilter = new TokenAuthenticationFilter();
-        http.addFilterBefore(tokenFilter, BasicAuthenticationFilter.class);
-
-        //Creating token when basic authentication is successful and the same token can be used to authenticate for further requests
-        final CustomBasicAuthenticationFilter customBasicAuthFilter = new CustomBasicAuthenticationFilter(this.authenticationManager());
-        http.addFilter(customBasicAuthFilter);
-
+        http
+                .exceptionHandling().and()
+                .anonymous().and()
+                .servletApi().and()
+                .headers().cacheControl().and().and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .anyRequest().hasAuthority(UserRoleEnum.ROLE_USER.getValue()).and()
+                .addFilterBefore(new TokenAuthenticationFilter(tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class);
     }
 }
---
+++ src/main/java/com/basicSpringBootRestAPI/config/TokenAuthenticationService.java
@@ -0,0 +1,64 @@
+package com.basicSpringBootRestAPI.config;
+
+import com.basicSpringBootRestAPI.entity.Role;
+import com.basicSpringBootRestAPI.entity.UserAuthenticationToken;
+import com.basicSpringBootRestAPI.repository.UserAuthenticationTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional(readOnly = true)
+public class TokenAuthenticationService {
+
+    @Autowired
+    private UserAuthenticationTokenRepository userAuthenticationTokenRepository;
+
+    public Authentication getAuthentication(HttpServletRequest request) {
+        System.out.println("TokenAuthenticationService -> getAuthentication");
+        
+        final String accessToken = request.getHeader("oauth-token");
+        if (accessToken != null) {
+            UserAuthenticationToken userAuthenticationToken = null;
+            try {
+                userAuthenticationToken = userAuthenticationTokenRepository.findByToken(accessToken);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            if (userAuthenticationToken == null) {
+                return null;
+            }
+
+            com.basicSpringBootRestAPI.entity.User user = userAuthenticationToken.getUser();
+
+            List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+            Set<Role> roles = user.getRoles();
+            for (Role role : roles) {
+                authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+            }
+
+            final User userSpring = new User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.isEnabled(),
+                    true,
+                    true,
+                    true,
+                    authorities);
+
+            return new UsernamePasswordAuthenticationToken(user, null, userSpring.getAuthorities());
+        }
+        return null;
+    }
+}
