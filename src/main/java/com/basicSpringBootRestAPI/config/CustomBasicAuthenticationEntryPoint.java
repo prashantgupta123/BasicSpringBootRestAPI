4391d253b85ce16bc756f6d24878b1ad373f531a
prashantgupta123 <prashantgupta036@gmail.com>
2017-12-20 16:21:07 +0530

    Added logs when security config run

--- src/main/java/com/basicSpringBootRestAPI/config/CustomBasicAuthenticationEntryPoint.java
+++ src/main/java/com/basicSpringBootRestAPI/config/CustomBasicAuthenticationEntryPoint.java
@@ -1,13 +1,26 @@
 package com.basicSpringBootRestAPI.config;
 
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class CustomBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
     @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        super.commence(request, response, authException);
+        System.out.println("CustomBasicAuthenticationEntryPoint -> commence");
+    }
+
+    @Override
     public void afterPropertiesSet() throws Exception {
+        System.out.println("CustomBasicAuthenticationEntryPoint -> afterPropertiesSet");
         String REALM = "Restful Spring Security Application";
         setRealmName(REALM);
         super.afterPropertiesSet();
